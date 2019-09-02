9dae725c87544fd6fb6a5f7287e0d14bd756f947
leili <476371581@qq.com>
2019-09-02 18:12:52 +0800

    a

--- webhook.js
+++ webhook.js
@@ -2,6 +2,7 @@ let http = require('http');
 let server = http.createServer((req,res)=>{
     if(req.method == 'POST' && req.url == '/webhook'){
         res.setHeader('Content-Type','application/json');
+        console.log('push detected');
         res.end(JSON.stringify({ok:true}));
     }else{
         res.end('Not Found');
